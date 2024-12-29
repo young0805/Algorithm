6868f51255fe3af0146626dd52ca07703e7db621
James park <53165953+young0805@users.noreply.github.com>
2024-12-30 04:42:21 +0900

    [Silver V] Title: 다리 놓기, Time: 44 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,17 @@
+import math
+
+def calculate_bridges(test_cases):
+    results = []
+    for n, m in test_cases:
+        # 조합을 계산: mCn
+        results.append(math.comb(m, n))
+    return results
+
+# 입력 받기
+t = int(input())
+test_cases = [tuple(map(int, input().split())) for _ in range(t)]
+
+results = calculate_bridges(test_cases)
+
+for result in results:
+    print(result)
